045f6853f765c788bf22f2b4ca8b8e2a6c60b720
Don de Rojas <derojasdonny@gmail.com>
2023-08-30 09:29:04 -0700

    use different library for dnd

--- issues-tracker-client/src/components/sprintBoardView.tsx
+++ issues-tracker-client/src/components/sprintBoardView.tsx
@@ -1,153 +1,116 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import { SprintBoard, Columns, Ticket } from '../styles'
-import { CSS } from "@dnd-kit/utilities";
-import { DndContext, closestCenter } from '@dnd-kit/core'
-import {
-  arrayMove,
-  SortableContext,
-  verticalListSortingStrategy,
-  useSortable
-} from "@dnd-kit/sortable";
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
-const SortableItem = (props: any) => {
-  // props.id
-  // JavaScript
-
-  const {
-      attributes,
-      listeners,
-      setNodeRef,
-      transform,
-      transition
-  } = useSortable({id: props.id});
-
-  const style = {
-      transform: CSS.Transform.toString(transform),
-      transition
-  }
-
-  return (
-      <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-          <Ticket>{props.id}</Ticket>
-      </div>
-  )
-}
-
+const isPositionChanged = (destination: any, source: any) => {
+  if (!destination) return false;
+  const isSameList = destination.droppableId === source.droppableId;
+  const isSamePosition = destination.index === source.index;
+  return !isSameList || !isSamePosition;
+};
 
 const SprintBoardView = (props:any) => {
-  const [languages, setLanguages ] = useState(["JavaScript", "Python", "TypeScript"]);
 
-  const handleDragEnd = (event:any) => {
-    console.log("Drag end called");
-    const {active, over} = event;
-    console.log("ACTIVE: " + active.id);
-    console.log("OVER :" + over.id);
+  const handleOnDragEnd = ({ draggableId, destination, source }: any) => {
+    console.log('in dragend!!!')
+    if (!isPositionChanged(source, destination)) return;
 
-    if(active.id !== over.id) {
-      setLanguages((items) => {
-        const activeIndex = items.indexOf(active.id);
-        const overIndex = items.indexOf(over.id);
-        console.log(arrayMove(items, activeIndex, overIndex));
-        return arrayMove(items, activeIndex, overIndex);
-        // items: [2, 3, 1]   0  -> 2
-        // [1, 2, 3] oldIndex: 0 newIndex: 2  -> [2, 3, 1] 
-      });
-      
-    }
-  }
+    const issueId = Number(draggableId);
+    console.log(issueId, 'issueId11')
+    // api.optimisticUpdate(`/issues/${issueId}`, {
+    //   updatedFields: {
+    //     status: destination.droppableId,
+    //     listPosition: calculateIssueListPosition(project.issues, destination, source, issueId),
+    //   },
+    //   currentFields: project.issues.find(({ id }) => id === issueId),
+    //   setLocalData: fields => updateLocalProjectIssues(issueId, fields),
+    // });
+  };
 
   return (
+    <DragDropContext onDragEnd={handleOnDragEnd}>
       <SprintBoard>
-        <DndContext
-          collisionDetection={closestCenter}
-          onDragEnd={handleDragEnd}
-        >
-          <Columns>
-            <SortableContext
-              items={languages}
-              strategy={verticalListSortingStrategy}
-            >
-              {/* We need components that use the useSortable hook */}
-              {languages.map(language => <SortableItem key={language} id={language}/>)}
-            </SortableContext>
-          </Columns>
-        </DndContext>
-        <DndContext
-          collisionDetection={closestCenter}
-          onDragEnd={handleDragEnd}
-        >
-          <Columns>
-            <SortableContext
-              items={languages}
-              strategy={verticalListSortingStrategy}
-            >
-              {/* We need components that use the useSortable hook */}
-              {languages.map(language => <SortableItem key={language} id={language}/>)}
-            </SortableContext>
-          </Columns>
-        </DndContext>
-        <DndContext
-          collisionDetection={closestCenter}
-          onDragEnd={handleDragEnd}
-        >
-          <Columns>
-            <SortableContext
-              items={languages}
-              strategy={verticalListSortingStrategy}
-            >
-              {/* We need components that use the useSortable hook */}
-              {languages.map(language => <SortableItem key={language} id={language}/>)}
-            </SortableContext>
-          </Columns>
-        </DndContext>
+        <Droppable key={'you were the chosen one!'} droppableId='you were the chosen one!'>
+          { provided => (
+            <Draggable draggableId={issue.id.toString()} index={index}>
+              {(provided, snapshot) => (
+                <Columns {...provided.droppableProps} ref={provided.innerRef}>
+                      <Ticket>
+                        <div>you were the chosen one!</div>
+                      </Ticket>
+                      {provided.placeholder}
+                </Columns>
+              )}
+            </Draggable>
+          )}
+        </Droppable>
+        <Droppable key={''} droppableId=''>
+          { provided => (
+            <Columns>
+                  <Ticket>
+                    <div>hello there</div>
+                  </Ticket>
+            </Columns>
+          )}
+        </Droppable>
+        <Droppable key={''} droppableId=''>
+          { provided => (
+            <Columns>
+                  <Ticket>
+                    <div>you will tryyy</div>
+                  </Ticket>
+            </Columns>
+          )}
+        </Droppable>
       </SprintBoard>
-    // <SprintBoard>
-  //     {/* <Columns>
-  //       {props.sprintBoardState.todo?.map((elem:any) => {
-  //         return (
-  //           <Ticket onClick={() => {
-  //             props.openModalWithData(elem.Assignee, elem.Description, elem.PriorityLevel, elem.TicketStatus, elem.IssueType, elem.TicketId)
-  //           }}>
-  //             <div>{elem.Assignee}</div>
-  //             <div>{elem.Description}</div>
-  //             <div>{elem.PriorityLevel}</div>
-  //             <div>{elem.IssueType}</div>
-  //             <div>{elem.TicketStatus}</div>
-  //           </Ticket>
-  //         )
-  //       })}
-  //     </Columns>
-  //     <Columns>
-  //       {props.sprintBoardState.inProgress?.map((elem:any) => {
-  //         return (
-  //           <Ticket onClick={() => {
-  //             props.openModalWithData(elem.Assignee, elem.Description, elem.PriorityLevel, elem.TicketStatus, elem.IssueType, elem.TicketId)
-  //           }}>
-  //             <div>{elem.Assignee}</div>
-  //             <div>{elem.Description}</div>
-  //             <div>{elem.PriorityLevel}</div>
-  //             <div>{elem.IssueType}</div>
-  //             <div>{elem.TicketStatus}</div>
-  //           </Ticket>
-  //         )
-  //       })}
-  //     </Columns>
-  //     <Columns>
-  //       {props.sprintBoardState.done?.map((elem:any) => {
-  //         return (
-  //           <Ticket onClick={() => {
-  //             props.openModalWithData(elem.Assignee, elem.Description, elem.PriorityLevel, elem.TicketStatus, elem.IssueType, elem.TicketId)
-  //           }}>
-  //             <div>{elem.Assignee}</div>
-  //             <div>{elem.Description}</div>
-  //             <div>{elem.PriorityLevel}</div>
-  //             <div>{elem.IssueType}</div>
-  //             <div>{elem.TicketStatus}</div>
-  //           </Ticket>
-  //         )
-  //       })}
-  //     </Columns> */}
-  //   {/* </SprintBoard> */}
+    </DragDropContext>
+        //   <SprintBoard>
+        //   <Columns>
+        //     {props.sprintBoardState.todo?.map((elem:any) => {
+        //       return (
+        //         <Ticket onClick={() => {
+        //           props.openModalWithData(elem.Assignee, elem.Description, elem.PriorityLevel, elem.TicketStatus, elem.IssueType, elem.TicketId)
+        //         }}>
+        //           <div>{elem.Assignee}</div>
+        //           <div>{elem.Description}</div>
+        //           <div>{elem.PriorityLevel}</div>
+        //           <div>{elem.IssueType}</div>
+        //           <div>{elem.TicketStatus}</div>
+        //         </Ticket>
+        //       )
+        //     })}
+        //   </Columns>
+        //   <Columns>
+        //     {props.sprintBoardState.inProgress?.map((elem:any) => {
+        //       return (
+        //         <Ticket onClick={() => {
+        //           props.openModalWithData(elem.Assignee, elem.Description, elem.PriorityLevel, elem.TicketStatus, elem.IssueType, elem.TicketId)
+        //         }}>
+        //           <div>{elem.Assignee}</div>
+        //           <div>{elem.Description}</div>
+        //           <div>{elem.PriorityLevel}</div>
+        //           <div>{elem.IssueType}</div>
+        //           <div>{elem.TicketStatus}</div>
+        //         </Ticket>
+        //       )
+        //     })}
+        //   </Columns>
+        //   <Columns>
+        //     {props.sprintBoardState.done?.map((elem:any) => {
+        //       return (
+        //         <Ticket onClick={() => {
+        //           props.openModalWithData(elem.Assignee, elem.Description, elem.PriorityLevel, elem.TicketStatus, elem.IssueType, elem.TicketId)
+        //         }}>
+        //           <div>{elem.Assignee}</div>
+        //           <div>{elem.Description}</div>
+        //           <div>{elem.PriorityLevel}</div>
+        //           <div>{elem.IssueType}</div>
+        //           <div>{elem.TicketStatus}</div>
+        //         </Ticket>
+        //       )
+        //     })}
+        //   </Columns>
+        // </SprintBoard>
   )
 }
 
