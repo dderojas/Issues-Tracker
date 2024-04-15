4b851f8b5064d70663aa8a05225e2ad0852a67cd
Don de Rojas <derojasdonny@gmail.com>
2024-04-15 09:40:45 -0700

    wip...

--- issues-tracker-client/src/components/index.ts
+++ issues-tracker-client/src/components/index.ts
@@ -1,4 +1,4 @@
-import Modal from '@components/Modal';
+import Modal from '@components/Modal.tsx';
 import BacklogView from "@components/BacklogView";
 import { KanbanBoardView } from "@components/KanbanBoardView";
 import { IssuesTracker } from "@components/IssuesTracker";
