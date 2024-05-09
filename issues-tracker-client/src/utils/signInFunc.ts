b04fd1df041f98fdd188bca945db94a1ed9b6475
Don de Rojas <derojasdonny@gmail.com>
2024-05-09 07:34:57 -0700

    wip

--- issues-tracker-client/src/utils/signInFunc.ts
+++ issues-tracker-client/src/utils/signInFunc.ts
@@ -1,8 +1,6 @@
-export const createSignInResponseObj = (response: any) => {
-  return {
+export const createSignInResponseObj = (response: any) => ({
     token: response.body.jwtToken,
     expiresIn: 3600,
     tokenType: "Bearer",
     authState: { email: response.body.results.Items[0].Username },
-  }
-}
+})
